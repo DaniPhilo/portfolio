0915e52ba1274b1783095b722ca2962c7875b78e
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-08-12 21:20:09 +0200

    New footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <section className='footer-section'>
+      <p className='copyright'>Designed & Built by Daniel Carrasco Casado  <span>&copy; 2022</span></p>
+      <p>This site uses <span>React & SASS</span> || Check the repository <a href='https://github.com/DaniPhilo/portfolio' target="_blank" rel="noopener noreferrer">here</a></p>
+    </section>
   )
 }
