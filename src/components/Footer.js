e6f0bc57e2223dbb2b04e69c40d02c9244a62bbc
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-08-17 19:17:58 +0200

    Footer width fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <section className='footer-section'>
+    <footer className='footer-section'>
       <p className='copyright'>Designed & Built by Daniel Carrasco Casado  <span>&copy; 2022</span></p>
       <p>This site uses <span>React & SASS</span> || Check the repository <a href='https://github.com/DaniPhilo/portfolio' target="_blank" rel="noopener noreferrer">here</a></p>
-    </section>
+    </footer>
   )
 }
