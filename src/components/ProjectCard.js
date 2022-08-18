dc41b98ccc12847257e9c5fc7d7ac8511a9f479b
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-08-18 19:57:17 +0200

    Added gh link to projects and img are now links to project pages

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,48 +3,54 @@ import React from 'react'
 export default function ProjectCard({ project, index }) {
 
     return (<>
-            {index % 2 === 0 ?
-                <div className='project-card-left'>
-                    <div className="project-img">
-                        <img src={project.img} alt="" />
+        {index % 2 === 0 ?
+            <div className='project-card-left'>
+                <div className="project-img">
+                    <a href=""><img src={project.img} alt="" /></a>
+                </div>
+                <div className="project-info">
+                    <div className="project-title">
+                        <h3>{project.name}</h3>
                     </div>
-                    <div className="project-info">
-                        <div className="project-title">
-                            <h3>{project.name}</h3>
-                        </div>
-                        <div className='project-description'>
-                            {project.description}
-                        </div>
-                        <div className="project-techs">
-                            <ul className="tech-list">
-                                {project.technologies.map((tech, index) => {
-                                    return <li key={index}>{tech}</li>
-                                })}
-                            </ul>
-                        </div>
+                    <div className='project-description'>
+                        {project.description}
+                    </div>
+                    <div className="project-techs">
+                        <ul className="tech-list">
+                            {project.technologies.map((tech, index) => {
+                                return <li key={index}>{tech}</li>
+                            })}
+                        </ul>
+                    </div>
+                    <div className='gh-project-link'>
+                        <a href={project.gh_link} target="_blank"><i className="fa-brands fa-github"></i></a>
                     </div>
                 </div>
-                :
-                <div className='project-card-right'>
-                    <div className="project-info">
-                        <div className="project-title">
-                            <h3>{project.name}</h3>
-                        </div>
-                        <div className='project-description'>
-                            {project.description}
-                        </div>
-                        <div className="project-techs">
-                            <ul className="tech-list">
-                                {project.technologies.map((tech, index) => {
-                                    return <li key={index}>{tech}</li>
-                                })}
-                            </ul>
-                        </div>
+            </div>
+            :
+            <div className='project-card-right'>
+                <div className="project-info">
+                    <div className="project-title">
+                        <h3>{project.name}</h3>
                     </div>
-                    <div className="project-img">
-                        <img src={project.img} alt="" />
+                    <div className='project-description'>
+                        {project.description}
+                    </div>
+                    <div className="project-techs">
+                        <ul className="tech-list">
+                            {project.technologies.map((tech, index) => {
+                                return <li key={index}>{tech}</li>
+                            })}
+                        </ul>
+                    </div>
+                    <div className='gh-project-link'>
+                        <a href={project.gh_link} target="_blank"><i className="fa-brands fa-github"></i></a>
                     </div>
                 </div>
-            }
-        </>)
+                <div className="project-img">
+                    <a href=""><img src={project.img} alt="" /></a>
+                </div>
+            </div>
+        }
+    </>)
 }
