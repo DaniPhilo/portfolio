5644b98492ae1d3cfa65295ad29c0eb0a086c05a
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-08-18 20:12:50 +0200

    Added links and hover effects to social icons

--- src/components/IconsSidebar.js
+++ src/components/IconsSidebar.js
@@ -5,7 +5,7 @@ export default function Icons_sidebar() {
     const toGitHub = () => {
         return window.open("https://github.com/DaniPhilo?tab=repositories");
     }
-    
+
     const toLinkedIn = () => {
         return window.open("https://www.linkedin.com/in/daniel-carrasco-casado/");
     }
@@ -14,8 +14,12 @@ export default function Icons_sidebar() {
         <aside>
 
             <ul className='icons-container'>
-                <li><i className="fa-brands fa-github" onClick={toGitHub}></i></li>
-                <li><i className="fa-brands fa-linkedin" onClick={toLinkedIn}></i></li>
+                <li>
+                    <a href="https://github.com/DaniPhilo" target="_blank"><i className="fa-brands fa-github" onClick={toGitHub}></i></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/daniel-carrasco-casado/" target="_blank"><i className="fa-brands fa-linkedin" onClick={toLinkedIn}></i></a>
+                </li>
                 <li><div id='icons-line'></div></li>
             </ul>
 
