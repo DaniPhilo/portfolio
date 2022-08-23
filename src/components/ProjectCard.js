15c9c21304d3fe6e1d57605fad96f97a92626ebd
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-08-23 20:24:00 +0200

    Responsive projects cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ProjectCard({ project, index }) {
 
+    const [windowSize, setWindowSize] = useState('');
+
+    const updateWindowWidth = () => {
+        setWindowSize(window.innerWidth);
+    }
+
+    const toProject = (url) => {
+        if (windowSize <= 550) {
+            window.open(url);
+        }
+        return
+    }
+
+    window.addEventListener('resize', updateWindowWidth);
+
     return (<>
         {index % 2 === 0 ?
-            <div className='project-card-left'>
+            <div className='project-card-left' onClick={() => toProject(project.project_link)}>
                 <div className="project-img">
                     <a href={project.project_link} target="_blank"><img src={project.img} alt="" /></a>
                 </div>
@@ -28,7 +43,7 @@ export default function ProjectCard({ project, index }) {
                 </div>
             </div>
             :
-            <div className='project-card-right'>
+            <div className='project-card-right' onClick={() => toProject(project.project_link)}>
                 <div className="project-info">
                     <div className="project-title">
                         <h3>{project.name}</h3>
