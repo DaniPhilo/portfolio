69554d9a5b1aee91d0e7e3c5468cd89dc741fea9
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-08-23 14:02:15 +0200

    Added hamburger btn and responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Header() {
+
+    const [windowSize, setWindowSize] = useState('');
+
+    const updateWindowWidth = () => {
+        setWindowSize(window.innerWidth);
+    }
+
     const [currentSection, setCurrentSection] = useState('home-section');
     let sections = [];
-    
+
     useEffect(() => {
         sections = document.querySelectorAll('section');
     }, []);
@@ -18,17 +25,32 @@ export default function Header() {
         });
     }
 
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(prevState => !prevState);
+    }
+
     window.addEventListener('scroll', changeSection);
+    window.addEventListener('resize', updateWindowWidth);
+
+
 
     return (
-        <header>
-            <ul>
-                <li><a href="#home-section" className={currentSection === 'home-section' ? 'active' : 'header-btn'}>home</a></li>
-                <li><a href="#about-section" className={currentSection === 'about-section' ? 'active' : 'header-btn'}>about</a></li>
-                <li><a href="#projects-section" className={currentSection === 'projects-section' ? 'active' : 'header-btn'}>projects</a></li>
-                <li><a href="#contact-section" className={currentSection === 'contact-section' ? 'active' : 'header-btn'}>contact</a></li>
-            </ul>
-        </header>
+        <>
+            <div className={`hamburger-container ${windowSize > 769 ? 'hide-btn' : 'show-btn'}`} onClick={toggleMenu}>
+                <div className="hamburger"></div>
+            </div>
 
+            <header className={`${windowSize > 769 ? 'full-header' : 'mobile-header'} ${showMenu ? 'display-menu' : 'hide-menu'}`}>
+
+                <ul>
+                    <li><a href="#home-section" className={currentSection === 'home-section' ? 'active' : 'header-btn'} onClick={toggleMenu}>home</a></li>
+                    <li><a href="#about-section" className={currentSection === 'about-section' ? 'active' : 'header-btn'} onClick={toggleMenu}>about</a></li>
+                    <li><a href="#projects-section" className={currentSection === 'projects-section' ? 'active' : 'header-btn'} onClick={toggleMenu}>projects</a></li>
+                    <li><a href="#contact-section" className={currentSection === 'contact-section' ? 'active' : 'header-btn'} onClick={toggleMenu}>contact</a></li>
+                </ul>
+            </header>
+        </>
     )
 }
